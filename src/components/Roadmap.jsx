795825a22e71810522c671ca5174aeb1dbe16933
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-30 03:23:35 +0530

    new

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Roadmap() {
   return (
-  <section className='  ' id='road'> 
-    <div className='container-wrapper py-16  '>
+    <section className="  " id="road">
+      <div className="container-wrapper py-16  ">
+        <img src="images/tar.png" alt="" className=" mx-auto h-[130px]" />
 
-
-      <div className=' relative '>
-
-        <div className=' flex items-center  gap-[20rem] -mb-[7.5%] relative z-40 '>
-        <img src="images/box-1.png" alt="" className=' h-[350px]' />
-          <img src="images/box-1.png" alt="" className=' h-[350px]' />
-
+        <div>
+          <h1 className="text-center text-2xl font-mon mb-2"> OUR ROADMAP</h1>
+          <div className=" bg-[#00ffc0] h-[4px] w-full max-w-[210px] mx-auto "></div>
         </div>
 
-        <img src="images/roadLine.png" alt="" />
-        <div className=' flex justify-end items-center  gap-[18rem] -mt-[7.5%] -mr-[4%]'>
-        <img src="images/box-2.png" alt="" className=' h-[350px]' />
-          <img src="images/box-2.png" alt="" className=' h-[350px]' />
+        <div className=" relative mt-[8%] ">
+          <div className=" flex items-center  gap-[20rem] -mb-[7.5%] relative z-40 ">
+            <img src="images/box-1.png" alt="" className=" h-[350px]" />
+            <img src="images/box-1.png" alt="" className=" h-[350px]" />
           </div>
 
+          <img src="images/roadLine.png" alt="" />
+          <div className=" flex justify-end items-center  gap-[18rem] -mt-[7.5%] -mr-[4%]">
+            <img src="images/box-2.png" alt="" className=" h-[350px]" />
+            <img src="images/box-2.png" alt="" className=" h-[350px]" />
+          </div>
+        </div>
       </div>
-
-      
-
-
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default Roadmap
+export default Roadmap;
