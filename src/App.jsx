384c733cbee84a21d07aa3dbc3bb394cdc01dd8f
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-30 03:19:00 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Roadmap from "./components/Roadmap";
 import Tokenomic from "./components/Tokenomic";
 
 
@@ -18,7 +19,7 @@ function App() {
       <Hero />
       <BorderSec />
      <Tokenomic/>
-
+      <Roadmap/>
       <Footer/>
       </div>
     </>
--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 function Roadmap() {
   return (
-  <section className=' road-bg -mt-[6%]' id='road'> 
-    <div className='container-wrapper pb-16  pt-[8%]'>
+  <section className='  ' id='road'> 
+    <div className='container-wrapper py-16  '>
 
-        <img src="images/roadmap.png" alt="" className=' mx-auto' />
+
+      <div className=' relative '>
+
+        <div className=' flex items-center  gap-[20rem] -mb-[7.5%] relative z-40 '>
+        <img src="images/box-1.png" alt="" className=' h-[350px]' />
+          <img src="images/box-1.png" alt="" className=' h-[350px]' />
+
+        </div>
+
+        <img src="images/roadLine.png" alt="" />
+        <div className=' flex justify-end items-center  gap-[18rem] -mt-[7.5%] -mr-[4%]'>
+        <img src="images/box-2.png" alt="" className=' h-[350px]' />
+          <img src="images/box-2.png" alt="" className=' h-[350px]' />
+          </div>
+
+      </div>
+
+      
 
 
     </div>
